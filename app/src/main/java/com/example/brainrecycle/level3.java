ff1a2e61d0a994fe88723fe92fa173f5e7215029
joaoleite2 <joao.cont3t@gmail.com>
2023-11-24 12:12:13 -0300

    código 4 bimestre finalizado

--- app/src/main/java/com/example/brainrecycle/level3.java
+++ app/src/main/java/com/example/brainrecycle/level3.java
@@ -3,20 +3,25 @@ package com.example.brainrecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class level3 extends AppCompatActivity {
     SensorManager sensorManager;
     Sensor sensor;
-    SensorEventListener sensorEventListener;
+    SensorEventListener evento;
     ImageView lampadaDesligada;
     ImageView lampadaLigada;
+
+    Integer mov=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,30 +32,36 @@ public class level3 extends AppCompatActivity {
         lampadaDesligada = findViewById(R.id.imgApagada);
         lampadaLigada = findViewById(R.id.imgAcesa);
 
-        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-    private class MySensorEventListener implements SensorEventListener {
-
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            acender(event);
+        if(sensor==null){
+            finish();
         }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        evento = new SensorEventListener() {
 
-        }
-    }
-
-    private void acender(SensorEvent event){
-        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            float x =event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-            if(Math.abs(x) > 1 || Math.abs(y) > 1 || Math.abs(z) > 1){
-                lampadaDesligada.setVisibility(View.GONE);
-                lampadaLigada.setVisibility(View.VISIBLE);
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0]<-4&&mov==0){
+                    mov++;
+                }else{
+                    if(sensorEvent.values[0]>4&&mov==1){
+                        mov++;
+                    }
+                }
+                if(mov==2){
+                    mov=0;
+                    lampadaDesligada.setVisibility(View.GONE);
+                    lampadaLigada.setVisibility(View.VISIBLE);
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse ("https://github.com/joaoleite2/BrainRecycle/tree/main")));
+                    Toast.makeText(getApplicationContext(),"Concluído. Nos acompanhe para saber as próximas novidades.",Toast.LENGTH_LONG).show();
+                }
             }
-        }
-    }
-}
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        sensorManager.registerListener(evento,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+    }}
